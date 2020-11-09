6157f033278b740f217e01552df767b8f068e2bc
aufafann <maufaaffan1335@gmail.com>
2020-11-09 14:35:21 +0700

    migration laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Clousure
+Route::get('/profil', function(){
+    return "ini adalah profil siswa";
+})->name('profil');
+
+//Route name
+Route::get("/tesname", function(){
+    return route('profil');
+});
+
+//parameter
+Route::get('/profil/{id}', function($id){
+    return $id;
+});
+
+//controller
+Route::get("/tescontroller", "TesController@show");
+
+//Resource
+Route::resource('siswa', "SiswaController"); 
