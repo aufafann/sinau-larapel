a2c3365928522af9b7ab129c48a76398e500f8ce
aufafann <maufaaffan1335@gmail.com>
2021-02-23 15:36:45 +0700

    Auth

--- resources/views/master/navigasi.blade.php
+++ resources/views/master/navigasi.blade.php
@@ -15,17 +15,13 @@
       <li class="nav-item">
         <a href="{{url ('/siswa')}}" class="nav-link navs">Data Siswa</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle navs" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+      <li class="nav-item">
+        <a href="{{url ('/login')}}" class="nav-link navs">login</a>
       </li>
+      <li class="nav-item">
+        <a href="{{url ('/logout')}}" class="nav-link navs">logout</a>
+      </li>
+      
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/login', 'AuthController@login')->name('login');
+Route::post('/login', 'AuthController@postLogin')->name('postLogin');
+Route::get('/logout', 'AuthController@logout')->name('logout');
+
+Route::group(['middleware' => 'auth'], function(){
+
 Route::get('/siswa', 'SiswaController@index')->name('siswa.view');
 
 Route::get('/siswa/siswatambah', 'SiswaController@create');
@@ -30,3 +29,6 @@ Route::get('/siswa/edit/{id}', 'SiswaController@edit')->name('siswa.edit');
 Route::put('/siswa/update/{id}', 'SiswaController@update')->name('siswa.update');
 
 Route::delete('/siswa/delete/{id}', 'SiswaController@delete')->name('siswa.delete');
+});
+
+
